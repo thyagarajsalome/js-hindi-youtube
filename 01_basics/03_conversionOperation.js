8321afcd5cfdff85f76e90f7406aab753efeb163
Thyagaraja M <142228812+thyagarajsalome@users.noreply.github.com>
2023-12-20 13:15:10 +0000

    conversion data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "thyagaraja"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+//"33"=> 33
+//"33abc" => NaN
+//true => 1; flase=> 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=> true; 0=> false
+//"hitesh"=> true
+
+let someNumber = 33
+let someNumber = string(someNumber)
+console.log(typeof stringNumber);
